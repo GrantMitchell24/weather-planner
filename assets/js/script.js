b18deab14e74fe21eec25f23596314fab8af7c0b
GrantMitchell24 <GrantMitchell2423@yahoo.com>
2023-10-23 17:13:30 -0500

    Lots of changes to weather planner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,131 @@
-var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={a578ab4f26c2e05e73c69c0d6adc6341}'
-var searchFormEl = document.querySelector('#search-form');
-var cityName = document.querySelector("city-name");
+var currentWeatherDiv = $("#currentWeatherDiv")
+var fiveDayMainDiv = $("#fiveDayMainDiv")
+var searchBtn = $("#searchBtn")
+var searchField = $("#searchField")
+var searchHistoryDiv = $("#serachHistoryDiv")
+var dateToday = dayjs();
+var startUpLoaded = false;
+var searchedCities = [];
 
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
 
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
 
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
+function LoadSearchHistory() {
+  searchHistoryDiv.empty();
+  searchedCities = JSON.parse(localStorage.getItem("Weather-Dashboard-Cities"));
+  if (searchedCities === null) {
+    searchedCities = [];
   }
+  for (var i = 0; i < searchedCities.length; i++) {
+    $(searchHistoryDiv).append(
+      $("<button></button>").attr("value", searchedCities[i]).addClass("btn btn-primary cityBtn").text(searchedCities[i])
+    )
+  }
+  $(searchHistoryDiv).append(
+    $("<button></button>").addClass("btn btn-primary clearHistoryBtn").text("Clear Search History")
+  )
 
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+  if (startUploaded === false && searchedCities.length > 0) {
+    startUpLoaded = true;
+    getCityInfo(searchedCities[0])
+  }
+}
+LoadSearchHistory();
 
-  location.assign(queryString);
+function search() {
+  getCityInfo(searchField.val().trim());
 }
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+function getCityInfo(city) {
+  let requestCity = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a578ab4f26c2e05e73c69c0d6adc6341'
 
-searchInputVal.addEventListener("click", function () {
-  let cityName = searchInputVal.value.trim();
-  console.log("city-name = ", cityName);
+  fetch(requestCity)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      if (data.cod === 200) {
+        currentWeatherDiv.empty();
+        fiveDayMainDiv.empty();
 
-  let cityName = JSON.parse(localStorage.getItem("city-name")) || [];
-  console.log("city-name = ", cityName);
-  }
-  cityName.push(cityName);
-  localStorage.setItem("city-name", JSON.stringify(cityName));
-});
+        currentWeather(data.name, Math.round(data.main.temp), Math.round(data.main.feels_like), Math.round(data.wind.speed), data.main.humidity, data.weather[0].icon)
+
+        generateFiveDayForecast(data.coord.lat, data.coord.long);
+
+        $(searchField).val("");
+
+        if (searchedCities.indexOf(data.name) > -1) {
+          var removedCity = searchedCities.splice(searchedCities.indexOf(data.name), 1);
+          removedCity = [];
+        }
+
+        searchedCities.unshift(data.ame)
+
+        while (searchedCities.length > 5) {
+          searchedCities.pop();
+        }
+
+        localStorageStorage.setItem("Weather-Dashboard-Cities", JSON.stringify(searchedCities));
+
+        loadSearchedHistory();
+
+      } else {
+        alert("Invalid City, Search Again")
+      }
+    })
+}
+
+function currentWeather(cityName, temp, feelsLike, wind, humidity, icon) {
+  $(currentWeatherDiv).append(
+    $("<h2></h2>").text(`${cityName} ${dateToday.format("(dddd, MMM, Do)")}`).append(
+      $("<img></img>").attr("src", `https://openweathermap.org/img/wn/${icon}@2x.png`).addClass("todayIcon").attr("alt", "Icon showing weather")
+    ),
+    $("<p></p>").text(`Temp: ${temp}\x80F`),
+    $("<p></p>").text(`feels Like: ${feelsLike}\x80F`),
+    $("<p></p>").text(`Wind: ${wind}mph`),
+    $("<p></p>").text(`Humidity: ${humidity}%`).addClass("last-p")
+  )
+}
+
+function generateFiveDayForecast(lat, lon) {
+
+  let requestFiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=a578ab4f26c2e05e73c69c0d6adc6341`
+
+  fetch(requestFiveDayForecast)
+  .then(function (resopnse) {
+    return repsonse.json();
+  })
+.then(function (data) {
+
+  let subDiv =$("<div></div>").attr("id", "fiveDaySubDiv").addClass("row")
+  let sectionHead = $("<h2></h2>").text("5-day Forecast")
+
+  for (i =5; i < 40; i += 8) {
+    let dayCard = $("<div></div>")
+    $(dayCard).append(
+      $("<p></p>").text(`${dateToday.add(i, "day").format("dddd, MMM Do")}`),
+      $("<img></img>").attr("src", `https://openweathermap.org/img.wn/${data.list[i].weather[0].icon}@2x.png`).attr("alt", "Icon showing weather"),
+      $("<p></p>").text(`Temp: ${Math.round(data.list[i].main.temp_max)}\x80F`),
+      $("<p></p>").text(`Wind: ${Math.round(data.list[i].wind.speed)}mph`),
+      $("<p></p>").text(`Humidity: ${data.list[i].main.humidity}%`)
+    )
+    $(dayCard).addClass("singleCard col-12 col-sm-2");
+    $(subDiv).append(dayCard);
+    $(fiveDayMainDiv).append(sectionHead, subDiv);
+}
+})
+}
+
+
+searchBtn.on("click", search)
+
+$("#searchHistoryDiv").on("click", ".clearHistoryBtn", function() {
+  searchHistoryDiv.empty()
+  searchedCities = []
+  localStorage.removeItem("Weather-Dashboard-Cities")
+})
+
+$("#searchHistoryDiv").on("click", ".cityBtn", function (){
+  getCityInfo(this.value)}
+)
+
